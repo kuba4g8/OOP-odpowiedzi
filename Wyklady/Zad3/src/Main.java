d861841eee1fd413a197fef787351c48ec4828e4
kuba4g8 <kuba.holdys@gmail.com>
2025-10-27 21:46:29 +0100

    Zadanie 5 - konstruktory (Wyklad)

--- Wyklady/Zad3/src/Main.java
+++ Wyklady/Zad3/src/Main.java
@@ -9,4 +9,9 @@ void main() {
     TextHelp.Helper.printSmth();
     // nazwa ta sama ale wykonanie inne:D
     InnyPckg.Helper.printSmth();
+    zadania.Zad1_DemoPakietow.main(null);
+    zadania.Zad2_StaticImport.main(null);
+    zadania.Zad3_KonfliktNazw.main(null);
+    zadania.Zad4_WildcardImport.main(null);
+    zadania.Zad5_Podpakiety.main(null);
 }
